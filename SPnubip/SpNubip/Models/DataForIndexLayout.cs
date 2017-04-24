3e44ae66ee3d987efb9c02daf9251822518ee6d8
KiselevK <kirill.kiselev220@gmail.com>
2017-04-24 20:08:59 +0300

    indexlayout changed

---
+++ SPnubip/SpNubip/Models/DataForIndexLayout.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpNubip.Models
+{
+    public class DataForIndexLayout
+    {
+        public IEnumerable<News> news;
+        public IEnumerable<Project> project;
+
+
+    }
+}
